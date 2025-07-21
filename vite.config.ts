0d4883dedf5eca7bf84232c604219d3dfe3e9be7
Jeremy Koulish <jeremy.koulish@gmail.com>
2025-07-21 13:39:57 -0400

    implement Aqua as subpackage; configure integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,44 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath } from 'node:url'
 import autoprefixer from 'autoprefixer'
+import path from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const filename = fileURLToPath(import.meta.url)
+const pathSegments = path.dirname(filename)
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(pathSegments, './src'),
+      '@aqua': path.resolve(pathSegments, './aqua'),
     },
   },
   css: {
     modules: {
-      scopeBehaviour: 'global'
+      scopeBehaviour: 'global',
     },
     postcss: {
-      plugins: [autoprefixer({})]
+      plugins: [autoprefixer({})],
     },
     preprocessorOptions: {
       scss: {
-        sourceMapIncludeSources: true
-      }
-    }
-  }
+        sourceMapIncludeSources: true,
+        additionalData(source: string, fp: string) {
+          if (path.extname(fp) === '.vue') {
+            return `@use "@aqua/aqua-vars.module.scss" as *;
+${source}
+            `
+          } else {
+            // Not a vue file, please use the SCSS module system directly
+            return source
+          }
+        },
+      },
+    },
+  },
 })
